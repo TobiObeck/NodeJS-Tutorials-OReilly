9aabcd18ac86ff09fabbf86ca2590e0329b569e0
Tobias Oberbeck <tobiasoberbeck@gmx.de>
2016-09-18 13:16:34 +0200

    Fehlerbehandlung für closed clients
    if(clientList[i].writable)
    
    UND
    
    client.on('error', function(e)
    {
    console.log(e);
    });

--- 2-1_telnetChat.js
+++ 2-1_telnetChat.js
@@ -16,16 +16,39 @@ chatServer.on('connection', function(client)
 		broadcast(data, client);
 	});
 
+	client.on('end', function()
+	{
+		clientList.splice(clientList.indexOf(client), 1);
+	});
+
+	client.on('error', function(e)
+	{
+		console.log(e);
+	});
+
 });
 
 function broadcast(message, client)
 {
+	var cleanup = [];
 	for(var i = 0; i < clientList.length; i++)
 	{
 		if(client !== clientList[i])
 		{
-			clientList[i].write(client.name + ': ' + message);
+			if(clientList[i].writable)
+			{
+				clientList[i].write(client.name + ': ' + message);
+			}
+			else
+			{
+				cleanup.push(clientList[i]);
+				clientList[i].destroy();
+			}
 		}
+	}//tote nodes aus der Schreibschleife entfernen, um Index nicht zu verwirren
+	for(i = 0; i < cleanup.length; i++)
+	{
+		clientList.splice(clientList.indexOf(cleanup[i]), 1);
 	}
 }
 
