e58787f092ea624dabfc1d6b4b1a98eb5d831f8f
Tobias Oberbeck <tobiasoberbeck@gmx.de>
2016-09-18 12:19:12 +0200

    simple Broadcast

--- 2-1_telnetChat.js
+++ 2-1_telnetChat.js
@@ -1,11 +1,23 @@
 var net = require('net');
 
 var chatServer = net.createServer();
+var clientList = [];
 
-chatServer.on('connection', function(client){
+chatServer.on('connection', function(client)
+{
 	client.write('Hallo!\n');
-	client.write('Bye!\n');
-	client.end();
+
+	clientList.push(client);
+
+	client.on('data', function(data)
+	{
+		console.log(data.toString());
+		for(var i = 0; i < clientList.length; i++)
+		{
+			clientList[i].write(data);
+		}
+	});
+
 });
 
 chatServer.listen(9000);
